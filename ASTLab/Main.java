25d9d6ca0fd4ea7a8f7581aee1414ddb806e1d92
adriannliu <62720754+adriannliu@users.noreply.github.com>
2024-10-01 16:20:47 -0400

    Add files via upload

---
+++ ASTLab/Main.java
@@ -0,0 +1,29 @@
+import environment.Environment;
+import parser.Parser;
+import scanner.*;
+
+import java.io.*;
+
+/**
+ * Runs testers for the AST lab using an input tester file
+ *
+ * @author  Adrian Liu
+ * @version 4/09/2024
+ */
+public class Main
+{
+    /**
+     * Tests the scanner and parser given input files
+     * @param args          info from the command line
+     * @throws IOException  IOException if FileIO goes wrong
+     */
+    public static void main(String[] args) throws IOException, ScanErrorException
+    {
+        FileInputStream fileInputStream = new FileInputStream(new File(
+                "/Users/24adrianl/Desktop/compilers/ASTLabAdrianLiu/src/astTest3.txt"));
+        Scanner scanner = new Scanner(fileInputStream);
+        Parser parser = new Parser(scanner);
+
+        parser.parseStatement().exec(new Environment());
+    }
+}
